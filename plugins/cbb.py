20fc5d1b7192c63d2ffc809d2b757d20c42f8095
TAKICHAN <87557652+BeeKingg@users.noreply.github.com>
2021-10-21 09:19:44 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Channel : @sugarmangolive'\n○ Channel : @pecandu_desah\n○ Channel : @sugarmangolive</b>",
+            text = f"<b>○  Channel : @rateAsupan</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
